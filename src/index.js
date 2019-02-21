4cc01c9f0b5fe9089b2c8edd6e9dc6674128e204
Jieyu You <jyou61@gatech.edu>
2019-02-20 23:44:37 -0600

    .

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//only one tree rather than a forest
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
